d944586204e56324f7c0ff7acf2822ed8fec7485
gastontecheradev <acorde.musica10@gmail.com>
2025-06-28 17:30:04 -0300

    Update Program.pas

--- Program.pas
+++ Program.pas
@@ -1,11 +1,17 @@
 Program Pascal;
+
 Var
   inicio, valor, k: Integer;
-  Begin
-inicio := 1; valor := inicio;
-For k := inicio to 3 do
+
 Begin
-   valor := valor + 2;
-   write (k, ' ', inicio, ' ', valor)
-End;
+  inicio := 1;
+  valor := inicio;
+
+  For k := inicio to 3 do
+  Begin
+    valor := valor + 2;
+    write(k, ' ', inicio, ' ', valor);
+  End;
+  Readln
 End.
+
