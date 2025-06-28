58c8835ac4a8035015f873dcee037b8e186c7fc5
gastontecheradev <acorde.musica10@gmail.com>
2025-06-28 17:25:19 -0300

    Update Program.pas

--- Program.pas
+++ Program.pas
@@ -1,8 +1,11 @@
 Program Pascal;
-
-var saludar : string;
-begin
-    saludar := 'Hola';
-
-    writeln(saludar)
-end.
+Var
+  inicio, valor, k: Integer;
+  Begin
+inicio := 1; valor := inicio;
+For k := inicio to 3 do
+Begin
+   valor := valor + 2;
+   write (k, ' ', inicio, ' ', valor)
+End;
+End.
