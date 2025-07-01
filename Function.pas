ef5588af827abbb026d742c4ea86aa4190b87d7b
gastontecheradev <acorde.musica10@gmail.com>
2025-07-01 20:52:22 -0300

    commit

---
+++ Function.pas
@@ -0,0 +1,23 @@
+program Pr7Ej4;
+var tum, num, temp : integer;
+
+procedure proc(a, b : integer; var c : integer);
+var aux : integer;
+begin
+    aux := a * b;
+    aux := aux + 1;
+    c := aux + a;
+    writeLn(a, b, c, aux)
+end;
+
+{ Programa principal }
+begin
+    tum := 1;
+    num := 2;
+    proc(tum, num, temp);
+    writeLn(temp);
+    tum := 0;
+    num := 1;
+    proc(tum, num, temp);
+    writeLn(temp)
+end.
