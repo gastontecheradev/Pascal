eb6b55669bf9c9c4c16a78990fadc95985b625f5
gastontecheradev <acorde.musica10@gmail.com>
2025-07-03 18:42:03 -0300

    a

---
+++ Procedimiento.pas
@@ -0,0 +1,13 @@
+Program Procedimiento;
+
+Procedure corrimiento(a, b, c: Integer);
+begin
+    writeln(c, a, b)
+end;
+
+{ Programa principal }
+begin
+    corrimiento(4, 1, 7);
+    readln
+end.
+
